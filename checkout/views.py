c6a3d51e72c163c6f41bad16ab66ee2c6f06b64d
Marc Hopkins <marchopkins1@outlook.com>
2024-06-04 13:39:32 +0000

    Add stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PNtg106QCvmWCXTR53YlWhTRmEmH7QIY8MbFzAROC12OTuoTtbnHYiIYdRmHKJ412dqMVVOMLENseTJIzWRiisP00ByUqejWc',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
